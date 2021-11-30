b09b17c10f20934a56d7faa78eca8641f06a619c
oleg <gusevoleg@gmail.com>
2021-11-30 13:11:52 +0300

    test

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ def main():
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='full_posts_tasks', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='full_posts_tasks', on_message_callback=callback, auto_ack=False)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
