d9bce545d9cdb9d197a01d7942dea791e37fc2b1
oleg <gusevoleg@gmail.com>
2021-12-01 17:44:50 +0300

    test

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ def main():
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
         # ch.basic_ack(delivery_tag=method.delivery_tag)
-        from random import randrange
-        if randrange(10) > 5:
-            ch.close()
+
     channel.basic_consume(queue='full_posts_tasks', on_message_callback=callback)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
