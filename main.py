d3b4c579278c26d9541621772b489e517a68cfd4
oleg <gusevoleg@gmail.com>
2021-12-09 09:15:45 +0300

    test

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
         print(" [x] Received %r" % body)
         # ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='full_posts_tasks', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='full_posts_tasks', on_message_callback=callback, auto_ack=False)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
